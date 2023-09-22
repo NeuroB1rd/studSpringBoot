8ff7231077f338cd799b7a51d48b85919c27bdf5
NearBirdSound <Andrey.lisenko01@gmail.com>
2023-09-22 19:39:47 +0300

    add logging

---
+++ src/main/java/com/example/demo/Controller.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+
+@RestController
+public class Controller {
+	@GetMapping("/api/current-time") 
+	public Date home() {
+		return new Date(System.currentTimeMillis());
+	}
+	
+	@PostMapping("/EnterFio") 
+	String insert(@Valid @RequestBody Fio postFio)
+	{
+	    return "Hello, " + postFio.toString() + " !";
+	}
+}
